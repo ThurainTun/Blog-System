9373af1f59496effa6b14a08393a0985ef8f3b09
ThurainTun <stp7112004@gmail.com>
2023-03-20 04:47:45 +0630

    second-commit

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,13 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('blogs');
+});
+
+Route::get('/blogs/{blog}',function ($filename){
+    $path=__DIR__."/../resources/blogs/$filename.html";
+    $blog=file_get_contents($path);
+    return view('blog',[
+        'blog'=>$blog
+    ]);
 });
